f1498986388cdaa0717003d462f1de8a70c6f40a
05675 <cole10133@gmail.com>
2020-01-29 23:28:45 +0900

    Cname取得方法を変更中

--- Program.cs
+++ Program.cs
@@ -30,159 +30,166 @@ namespace jrascraping
             //レース結果のトップページのhtmlを取得する
             string html = new Downloder().GetHtml("pw01sli00/AF");
 
-            //取得したレース結果のTOPページから、レース名:cnameの組み合わせを正規表現で取得しテーブルに格納する
-            Dictionary<string, string> table = ParseRaceLinkTable(html);
-
-            //SQLiteのファイルを選択
-            string db_file = "Jra.db";
-            using SQLiteConnection conn = new SQLiteConnection("Data Source=" + db_file);
-
-            //CnameTable
-            foreach(var cname in table)
+            //取得したレース結果のTOPページから、レース日:cnameの組み合わせを正規表現で取得しテーブルに格納する
+            Dictionary<string, string> raceDays = ParseRacedayLinkTable(html);
+            foreach (var RaceDayPair in raceDays)
             {
-                var cnametabel = new Models.CnameTable()
+                string raceDayPage = new Downloder().GetHtml(RaceDayPair.Value);
+                //取得したレース結果のTOPページから、レース名:cnameの組み合わせを正規表現で取得しテーブルに格納する
+                Dictionary<string, string> table = ParseRaceLinkTable(html);
+
+
+                //SQLiteのファイルを選択
+                string db_file = "Jra.db";
+                using SQLiteConnection conn = new SQLiteConnection("Data Source=" + db_file);
+
+                //CnameTable
+                foreach (var cname in table)
                 {
-                    Racename = cname.Key,
-                    Cname = cname.Value
-                };
-                context.CnameTable.Add(cnametabel);
-            }
-            context.SaveChanges();
-
-            //RaceResults
-            foreach (var pair in table)
-            {
-                // 正規表現で取得したcnameを使用して、個別のレース結果のhtmlを得る
-                var raceResultHtml = new Downloder().GetHtml(pair.Value);
-
-                //年月日
-                Regex RegexDate = new Regex("(?<=<span class=\"opt\">).*?(?=（.*</span>)",
-                    RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                var MatchesDate = RegexDate.Matches(raceResultHtml);
-
-                //レース名
-
-
-                //着順
-                Regex RegexPlace = new Regex("(?<=<td class=\"place\">).*?(?=</td>)",
-                   RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                var MatchesPlace = RegexPlace.Matches(raceResultHtml);
-
-                //枠
-                Regex RegexWaku = new Regex("(?<=<td class=\"waku\"><img src=\"/JRADB/img/waku/).*?(?=.png\" alt=\")",
-                   RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                var MatchesWaku = RegexWaku.Matches(raceResultHtml);
-
-                foreach (Match place in MatchesPlace)
-                {
-                    foreach (Match Date in MatchesDate)
+                    var cnametabel = new Models.CnameTable()
                     {
-                        foreach (Match Waku in MatchesWaku)
+                        Racename = cname.Key,
+                        Cname = cname.Value
+                    };
+                    context.CnameTable.Add(cnametabel);
+                }
+                context.SaveChanges();
+
+                //RaceResults
+                foreach (var pair in table)
+                {
+                    // 正規表現で取得したcnameを使用して、個別のレース結果のhtmlを得る
+                    var raceResultHtml = new Downloder().GetHtml(pair.Value);
+
+                    //年月日
+                    Regex RegexDate = new Regex("(?<=<span class=\"opt\">).*?(?=（.*</span>)",
+                        RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    var MatchesDate = RegexDate.Matches(raceResultHtml);
+
+                    //レース名
+                    var RaceName = pair.Key;
+    
+                //着順
+                    Regex RegexPlace = new Regex("(?<=<td class=\"place\">).*?(?=</td>)",
+                   RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    var MatchesPlace = RegexPlace.Matches(raceResultHtml);
+
+                    //枠
+                    Regex RegexWaku = new Regex("(?<=<td class=\"waku\"><img src=\"/JRADB/img/waku/).*?(?=.png\" alt=\")",
+                       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    var MatchesWaku = RegexWaku.Matches(raceResultHtml);
+
+                    foreach (Match place in MatchesPlace)
+                    {
+                        foreach (Match Date in MatchesDate)
                         {
-                            Debug.WriteLine(Date);
-                            Debug.WriteLine(place);
-                            Debug.WriteLine(Waku);
+                            foreach (Match Waku in MatchesWaku)
+                            {
+                                Debug.WriteLine(Date);
+                                Debug.WriteLine(place);
+                                Debug.WriteLine(Waku);
+                            }
                         }
                     }
+
+                    foreach (Match waku in MatchesWaku)
+                    {
+                        Debug.WriteLine(waku);
+                        //cmd.CommandText = "INSERT INTO RaceResults (place,racename) VALUES (@place,@racename)";
+                        //cmd.Parameters.Add("place", System.Data.DbType.String);
+                        //cmd.Parameters["place"].Value = waku;
+                        //cmd.ExecuteNonQuery();
+                    }
+                    //    //馬番
+                    //    Regex RegexNum = new Regex("(?<=<td class=\"num\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesNum = RegexNum.Matches(raceResultHtml);
+                    //    foreach (Match num in MatchesNum)
+                    //    {
+                    //        //Debug.WriteLine(num);
+                    //    }
+
+                    //    //馬名
+                    //    Regex RegexHorse = new Regex("(?<=\\('/JRADB/accessU.html','pw.{20,20}'\\);\">).*?(?=</a>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesHorse = RegexHorse.Matches(raceResultHtml).Cast<Match>().ToList();
+                    //    for (int i = 0; i < MatchesPlace.Count; i++)
+                    //    {
+                    //        cmd.Parameters.Add("horse", System.Data.DbType.String);
+                    //        cmd.Parameters["horse"].Value = MatchesHorse[i].Value;
+                    //        cmd.CommandText = "update RaceResults set horse=@horse where racename = '" + pair.Key + "' and place= " + (i + 1);
+                    //        cmd.ExecuteNonQuery();
+                    //    }
+                    //    cmd.ExecuteNonQuery();
+                    //    //性齢
+                    //    Regex RegexAge = new Regex("(?<=<td class=\"age\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesAge = RegexAge.Matches(raceResultHtml);
+                    //    foreach (Match Age in MatchesAge)
+                    //    {
+                    //        //Debug.WriteLine(Age);
+                    //    }
+                    //    //負担重量
+                    //    Regex RegexWeight = new Regex("(?<=<td class=\"weight\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesWeight = RegexWeight.Matches(raceResultHtml);
+                    //    foreach (Match Weight in MatchesWeight)
+                    //    {
+                    //        //Debug.WriteLine(Weight);
+                    //    }
+                    //    //騎手
+                    //    Regex RegexJockey = new Regex("(?<=\\('/JRADB/accessK.html','pw.{14,14}'\\);\">).*?(?=</a>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesJockey = RegexJockey.Matches(raceResultHtml);
+                    //    foreach (Match Jockey in MatchesJockey)
+                    //    {
+                    //        //Debug.WriteLine(Jockey);
+                    //    }
+                    //    //タイム
+                    //    Regex RegexTime = new Regex("(?<=<td class=\"time\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesTime = RegexTime.Matches(raceResultHtml);
+                    //    foreach (Match Time in MatchesTime)
+                    //    {
+                    //        //Debug.WriteLine(Time);
+                    //    }
+                    //    //着差※
+                    //    Regex RegexMargin = new Regex("(?<=<td class=\"margin\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesMargin = RegexMargin.Matches(raceResultHtml);
+                    //    foreach (Match Margin in MatchesMargin)
+                    //    {
+                    //        //Debug.WriteLine(Margin);
+                    //    }
+                    //    //コーナー、通過順位※
+
+                    //    //推定上り
+                    //    Regex RegexFtime = new Regex("(?<=<td class=\"f_time\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesFtime = RegexFtime.Matches(raceResultHtml);
+                    //    foreach (Match Ftime in MatchesFtime)
+                    //    {
+                    //        //Debug.WriteLine(Ftime);
+                    //    }
+                    //    //馬体重※
+
+                    //    //調教師
+                    //    Regex RegexTrainer = new Regex("(?<=\\('/JRADB/accessC.html','pw.{14,14}'\\);\">).*?(?=</a>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesTrainer = RegexTrainer.Matches(raceResultHtml);
+                    //    foreach (Match Trainer in MatchesTrainer)
+                    //    {
+                    //        //Debug.WriteLine(Trainer);
+                    //    }
+                    //    //単勝人気
+                    //    Regex RegexPop = new Regex("(?<=<td class=\"pop\">).*?(?=</td>)",
+                    //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    //    var MatchesPop = RegexPop.Matches(raceResultHtml);
+                    //    foreach (Match Pop in MatchesPop)
+                    //    {
+                    //        //Debug.WriteLine(Pop);
+                    //    }
                 }
-
-                foreach (Match waku in MatchesWaku)
-                {
-                    Debug.WriteLine(waku);
-                    //cmd.CommandText = "INSERT INTO RaceResults (place,racename) VALUES (@place,@racename)";
-                    //cmd.Parameters.Add("place", System.Data.DbType.String);
-                    //cmd.Parameters["place"].Value = waku;
-                    //cmd.ExecuteNonQuery();
-                }
-                //    //馬番
-                //    Regex RegexNum = new Regex("(?<=<td class=\"num\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesNum = RegexNum.Matches(raceResultHtml);
-                //    foreach (Match num in MatchesNum)
-                //    {
-                //        //Debug.WriteLine(num);
-                //    }
-
-                //    //馬名
-                //    Regex RegexHorse = new Regex("(?<=\\('/JRADB/accessU.html','pw.{20,20}'\\);\">).*?(?=</a>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesHorse = RegexHorse.Matches(raceResultHtml).Cast<Match>().ToList();
-                //    for (int i = 0; i < MatchesPlace.Count; i++)
-                //    {
-                //        cmd.Parameters.Add("horse", System.Data.DbType.String);
-                //        cmd.Parameters["horse"].Value = MatchesHorse[i].Value;
-                //        cmd.CommandText = "update RaceResults set horse=@horse where racename = '" + pair.Key + "' and place= " + (i + 1);
-                //        cmd.ExecuteNonQuery();
-                //    }
-                //    cmd.ExecuteNonQuery();
-                //    //性齢
-                //    Regex RegexAge = new Regex("(?<=<td class=\"age\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesAge = RegexAge.Matches(raceResultHtml);
-                //    foreach (Match Age in MatchesAge)
-                //    {
-                //        //Debug.WriteLine(Age);
-                //    }
-                //    //負担重量
-                //    Regex RegexWeight = new Regex("(?<=<td class=\"weight\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesWeight = RegexWeight.Matches(raceResultHtml);
-                //    foreach (Match Weight in MatchesWeight)
-                //    {
-                //        //Debug.WriteLine(Weight);
-                //    }
-                //    //騎手
-                //    Regex RegexJockey = new Regex("(?<=\\('/JRADB/accessK.html','pw.{14,14}'\\);\">).*?(?=</a>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesJockey = RegexJockey.Matches(raceResultHtml);
-                //    foreach (Match Jockey in MatchesJockey)
-                //    {
-                //        //Debug.WriteLine(Jockey);
-                //    }
-                //    //タイム
-                //    Regex RegexTime = new Regex("(?<=<td class=\"time\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesTime = RegexTime.Matches(raceResultHtml);
-                //    foreach (Match Time in MatchesTime)
-                //    {
-                //        //Debug.WriteLine(Time);
-                //    }
-                //    //着差※
-                //    Regex RegexMargin = new Regex("(?<=<td class=\"margin\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesMargin = RegexMargin.Matches(raceResultHtml);
-                //    foreach (Match Margin in MatchesMargin)
-                //    {
-                //        //Debug.WriteLine(Margin);
-                //    }
-                //    //コーナー、通過順位※
-
-                //    //推定上り
-                //    Regex RegexFtime = new Regex("(?<=<td class=\"f_time\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesFtime = RegexFtime.Matches(raceResultHtml);
-                //    foreach (Match Ftime in MatchesFtime)
-                //    {
-                //        //Debug.WriteLine(Ftime);
-                //    }
-                //    //馬体重※
-
-                //    //調教師
-                //    Regex RegexTrainer = new Regex("(?<=\\('/JRADB/accessC.html','pw.{14,14}'\\);\">).*?(?=</a>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesTrainer = RegexTrainer.Matches(raceResultHtml);
-                //    foreach (Match Trainer in MatchesTrainer)
-                //    {
-                //        //Debug.WriteLine(Trainer);
-                //    }
-                //    //単勝人気
-                //    Regex RegexPop = new Regex("(?<=<td class=\"pop\">).*?(?=</td>)",
-                //       RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                //    var MatchesPop = RegexPop.Matches(raceResultHtml);
-                //    foreach (Match Pop in MatchesPop)
-                //    {
-                //        //Debug.WriteLine(Pop);
-                //    }
             }
                 //trans.Commit();
         }
