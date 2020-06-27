7b414a68a6443da65edcede97e843bdd3816f0d4
ta kuzuo <j.cole.10@hotmail.co.jp>
2020-06-27 12:19:04 +0900

    フォルダ構成の変更

---
+++ XUnitTestJra/UnitTestJra.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestJra
+{
+    public class UnitTestJra
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Hello Test!");
+        }
+    }
+}
